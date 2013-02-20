7d61b60f612e1aede10f0b52e0ef060867772729
Fran <francisco.aisa.garcia@gmail.com>
2013-02-20 15:58:33 +0100

    Comentadas todos los componentes de disparo y organizada la jerarquia para comenzar a implementar las armas que disparan proyectiles.

--- Src/Logic/Entity/Components/ShootGrenadeLauncher.h
+++ Src/Logic/Entity/Components/ShootGrenadeLauncher.h
@@ -1,11 +1,17 @@
+//---------------------------------------------------------------------------
+// ShootMiniGun.h
+//---------------------------------------------------------------------------
+
 /**
-@file ShootGrenadeLauncher.h
+@file ShootMiniGun.h
+
+Contiene la declaración del componente que implementa la minigun.
 
 Contiene la declaración de la cabecera del componente
 de disparo del lanzagranadas.
 
 @see Logic::CShootGrenadeLauncher
-@see Logic::IComponent
+@see Logic::CShootProjectile
 
 @author Francisco Aisa García
 @date Febrero, 2013
@@ -14,24 +20,31 @@ de disparo del lanzagranadas.
 #ifndef __Logic_ShootGrenadeLauncher_H
 #define __Logic_ShootGrenadeLauncher_H
 
-#include "Logic/Entity/Components/Shoot.h"
+#include "Logic/Entity/Components/ShootProjectile.h"
 
 namespace Logic {
 
 	/**
     @ingroup logicGroup
 
+	Este componente implementa la funcionalidad del lanzagranadas.
+
 	@author Francisco Aisa García
 	@date Febrero, 2013
 	*/
 
-	class CShootGrenadeLauncher : public CShoot {
+	class CShootGrenadeLauncher : public CShootProjectile {
 		DEC_FACTORY(CShootGrenadeLauncher);
 
 	public:
-		CShootGrenadeLauncher() : CShoot("GrenadeLauncher") { }
 
-		virtual void shoot() { }
+		/** Constructor por defecto. */
+		CShootGrenadeLauncher() : CShootProjectile("GrenadeLauncher") { }
+
+		//__________________________________________________________________
+
+		/** Al ejecutarse dispara una granada. */
+		virtual void fireWeapon() { }
 	};
 
 	REG_FACTORY(CShootGrenadeLauncher);
