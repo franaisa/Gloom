f9d62361234939a65536256a266143204f14fb72
Fran <francisco.aisa.garcia@gmail.com>
2013-03-06 11:17:38 +0100

    Jodete git

--- Src/Physics/Server.cpp
+++ Src/Physics/Server.cpp
@@ -274,7 +274,7 @@ void CServer::createScene () {
 	PxSceneDesc sceneDesc(_physics->getTolerancesScale());
 
 	// Establecer la gravedad en el eje Y
-	sceneDesc.gravity = PxVec3(0.0f, -9.81f * 5, 0.0f);
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f * 7, 0.0f);
 
 	// Establecer el gestor de colisiones
 	sceneDesc.simulationEventCallback = _collisionManager;
