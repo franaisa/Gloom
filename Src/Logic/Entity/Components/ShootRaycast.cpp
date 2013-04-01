7097a330fa9103bee8da555d2f50aebb42678fb4
rub0 <rub0te@gmail.com>
2013-04-01 20:42:23 +0200

    comentado el dibujado de particulas porque me peta :(

--- Src/Logic/Entity/Components/ShootRaycast.cpp
+++ Src/Logic/Entity/Components/ShootRaycast.cpp
@@ -59,7 +59,7 @@ namespace Logic {
 			_canShoot = false;
 			_cooldownTimer = 0;
 				
-			drawParticle("fire", "ShootParticle");
+			//drawParticle("fire", "ShootParticle");
 
 			for(int i = 0; i < _numberShots; ++i) {
 				std::pair<CEntity*, Ray> entityHit = fireWeapon();
