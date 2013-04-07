4954464bf086daf0ab426f42b2fc6c10104a200d
Antonio <estopero88@gmail.com>
2013-04-07 14:18:39 +0200

    mejorando el debug overlay

---
+++ Src/Logic/Messages/MessageHudDebugData.h
@@ -0,0 +1,30 @@
+#ifndef __Logic_MessageHudDebugData_H
+#define __Logic_MessageHudDebugData_H
+
+#include "Message.h"
+
+namespace Logic {
+
+	class CMessageHudDebugData: public CMessage{
+	DEC_FACTORYMESSAGE(CMessageHudDebugData);
+	public:
+		CMessageHudDebugData();
+		~CMessageHudDebugData(){};
+
+		void setKey(std::string key);
+		void setValue(std::string value);
+
+		std::string getKey();
+		std::string getValue();
+		
+		virtual Net::CBuffer* serialize();
+		virtual void deserialize(Net::CBuffer& buffer);
+	private:
+
+		std::string _key;
+		std::string _value;
+	};
+	REG_FACTORYMESSAGE(CMessageHudDebugData);
+};
+
+#endif
