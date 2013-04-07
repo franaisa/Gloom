f2433a272b01e3ebf6cd0eb388e9fec5bb16c2fc
Jaime <jaime.chapinal@gmail.com>
2013-04-07 15:44:14 +0200

    Nuevo modelo jumper. Lava de fondo en creacion.shader screamer

--- Exes/media/materials/programs/shieldCone.hlsl
+++ Exes/media/materials/programs/shieldCone.hlsl
@@ -20,10 +20,12 @@ VsOutput vsMain(const VsInput In) {
 	VsOutput vsOut;
 	float4 pos = In.position;
 	float random = clamp(tiempo,1, 5);
-	//pos.x += (sin(pos.y+tiempo*random) + sin(pos.z + tiempo*random))*0.3;
 	pos.x += In.normal.x * sin(tiempo*4) * 0.3;
 	pos.y += In.normal.y * sin(tiempo*4) * 0.3;
 	pos.z += In.normal.z * sin(tiempo*4) * 0.3;
+		
+	pos.x += (sin(pos.y+tiempo*random) + sin(pos.z + tiempo*random))*0.05;
+
 	vsOut.position = mul(ViewProjectionMatrix, pos);
 	vsOut.uv0 = In.uv0;
 	//uv0.a=0.5;
