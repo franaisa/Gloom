726c7a2b0e1a0fa4b1f4fae746d51ccb3cd91f80
rub0 <rub0te@gmail.com>
2013-04-22 12:18:24 +0200

    - Tweaking infinito de los movimientos, ahora con un feeling muchisimo   mejor, pero a fran no le gusta todavia (te odio, pareces un diseñador ¬¬)

--- Src/Logic/Entity/Components/AvatarController.cpp
+++ Src/Logic/Entity/Components/AvatarController.cpp
@@ -24,9 +24,9 @@ de la entidad.
 namespace Logic {
 	IMP_FACTORY(CAvatarController);
 
-	CAvatarController::CAvatarController() : _gravity(0, -0.005f, 0),
+	CAvatarController::CAvatarController() : _gravity(0, -0.0003f, 0),
 											 _maxFallSpeed(-2.0f),
-											 _airFrictionCoef(0.9f),
+											 _airFrictionCoef(0.98f),
 											 _touchingGround(false) {
 		// Inicializamos el array que contiene los vectores
 		// de cada tecla de movimiento
@@ -43,7 +43,7 @@ namespace Logic {
 
 		assert( entityInfo->hasAttribute("maxVelocity") && "Error: No se ha definido el atributo maxVelocity en el mapa" );
 		_maxVelocity = entityInfo->getFloatAttribute("maxVelocity");
-		_maxGravVelocity = _maxVelocity*2;
+		_maxGravVelocity = _maxVelocity*1.5;
 		return true;
 
 	} // spawn
@@ -116,14 +116,9 @@ namespace Logic {
 		IComponent::tick(msecs);
 
 		// Calculamos el vector de desplazamiento
-		//Vector3 displacement = _touchingGround ? estimateGroundMotion(msecs) : estimateAirMotion(msecs);
-		Vector3 displacement = estimateGroundMotion(msecs);
+		Vector3 displacement = _touchingGround ? estimateGroundMotion(msecs) : estimateAirMotion(msecs);
+		//Vector3 displacement = estimateGroundMotion(msecs);
 
-		//aplicamos la gravedad (solo si no estamos cayendo)
-		if(_touchingGround)
-			_momentum.y=_gravity.y*msecs;
-		else
-			_momentum.y+=_gravity.y*msecs;
 
 		// Tratamos de mover el controlador fisico con el desplazamiento estimado.
 		// En caso de colision, el controlador fisico nos informa.
@@ -181,26 +176,33 @@ namespace Logic {
 	//________________________________________________________________________
 
 	Vector3 CAvatarController::estimateGroundMotion(unsigned int msecs) {
-		float coef = 13.0f/(double)msecs;
+		float coef =1;
+		if(_displacementDir==Vector3::ZERO)
+			coef = 15.0f/(double)msecs;
+		std::cout << coef << std::endl;
 		_momentum*=Vector3(coef,1,coef);
 		//_momentum*=0.95f;
-		Vector3 displacement = estimateMotionDirection() * _acceleration * msecs;
-		_momentum+= displacement * Vector3(1,0,1);
 
+
+
+		Vector3 displacement = estimateMotionDirection() * accel * msecs * msecs * 0.5f;
+		_momentum+= displacement * Vector3(1,0,1);
+		_momentum.y=_gravity.y*msecs;
 		return _momentum;
 	}
 
 	//________________________________________________________________________
 
 	Vector3 CAvatarController::estimateAirMotion(unsigned int msecs) {
-		_momentum *= _airFrictionCoef;
-		if(_momentum.y > _maxFallSpeed) {
-			_momentum += _gravity * msecs;
+		
+		_momentum*=Vector3(_airFrictionCoef,1,_airFrictionCoef);
+		//_momentum*=0.95f;
+		Vector3 displacement = Vector3(3.0f/(double)msecs,0,3.0f/(double)msecs) * estimateMotionDirection() * _acceleration * msecs * msecs * 0.5f;
+		_momentum+= displacement;
 
-			if(_momentum.y < _maxFallSpeed) {
-				_momentum.y = _maxFallSpeed;
-			}
-		}
+
+
+		_momentum.y+=_gravity.y*msecs*msecs * 0.5f;
 
 		//std::cout << _momentum.y << std::endl;
 
@@ -215,7 +217,7 @@ namespace Logic {
 		if(momVelocity >_maxVelocity){
 			double coef = _maxVelocity/momVelocity;
 			_momentum*=Vector3(coef,1,coef);
-			//std::cout << "normalizo" << std::endl;
+			
 		}
 
 		//normalizamos la velocidad vertical
