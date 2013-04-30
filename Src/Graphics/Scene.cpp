5f446e63a68503c3bb31977b53aaf84ac745b26b
Antonio <estopero88@gmail.com>
2013-05-01 00:58:02 +0200

    cambiada la camara a como estaba en un principio, e intento de añadir un shader, lo subo por lo de la camara, que se que jaime esta liao con ello.

--- Src/Graphics/Scene.cpp
+++ Src/Graphics/Scene.cpp
@@ -134,6 +134,9 @@ namespace Graphics
 		_compositorManager->addCompositor(_camera->getOgreCamera()->getViewport(), "Motion Blur");
 		_compositorManager->setCompositorEnabled(_camera->getOgreCamera()->getViewport(), "Motion Blur", true);
 
+		_compositorManager->addCompositor(_camera->getOgreCamera()->getViewport(), "berserkCompositor");
+		_compositorManager->setCompositorEnabled(_camera->getOgreCamera()->getViewport(), "berserkCompositor", true);
+
 		_glowMaterialListener = new GlowMaterialListener();
 		Ogre::MaterialManager::getSingletonPtr()->addListener(_glowMaterialListener);
 
