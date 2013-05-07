a3fbbf3b9f58dc381b93796d67c91fb0aa7488da
Jaime <jaime.chapinal@gmail.com>
2013-05-08 01:32:52 +0200

    Flechica de danyo. Mensaje creado para comunicar CameraFeedbackNotifier con el angulo de la direccion entre jugador y enemigo y HudOverlay. Falta por poner la misma referencia de 0 grados en ambos y probar en casos extremos, como por ejemplo angulos justo de 90,180,etc.

---
+++ Src/Logic/Messages/MessageImpact.h
@@ -0,0 +1,33 @@
+#ifndef __Logic_MessageImpact_H
+#define __Logic_MessageImpact_H
+
+#include "Message.h"
+
+namespace Logic {
+
+	class CMessageImpact: public CMessage{
+	DEC_FACTORYMESSAGE(CMessageImpact);
+	public:
+		CMessageImpact();
+		virtual ~CMessageImpact();
+
+		CEntity* getRelatedEntity();
+
+		float getDirectionImpact		() { return _fAngleImpactDirection; }
+		void	setDirectionImpact		(float fAngle);
+		
+		virtual Net::CBuffer* serialize();
+		virtual void deserialize(Net::CBuffer& buffer);
+
+	private:
+		CEntity* _entity;
+
+		/**
+			Angulo con la dirección del impacto
+		*/
+		float	_fAngleImpactDirection; 
+	};
+	REG_FACTORYMESSAGE(CMessageImpact);
+};
+
+#endif
