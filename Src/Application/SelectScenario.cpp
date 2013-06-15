3809fe6386a0dc33957d20545d386f9a9f7af963
Antonio <estopero88@gmail.com>
2013-06-15 05:38:37 +0200

    me rindo por ahora U.U si alguien quiere intentarlo adelante. Con sus castas toas. No he probao el mapa fisico, me he peleao con el grafico. Un saludo

--- Src/Application/SelectScenario.cpp
+++ Src/Application/SelectScenario.cpp
@@ -164,7 +164,7 @@ namespace Application {
 		while (FindNextFile(hFind, &FindData))
 		{     
 			filename = FindData.cFileName;
-			if(filename == "map2_espectador.txt" || filename == "map2.txt" || filename == "map1.txt" || filename == "scenarioTest.txt")
+			if(filename == "map3_espectador.txt" || filename == "map3.txt" || filename == "map2_espectador.txt" || filename == "map2.txt" || filename == "map1.txt" || filename == "scenarioTest.txt")
 				_menu->callFunction("pushFile",Hikari::Args(filename.substr(0,filename.find(".txt"))));
 
 		}
