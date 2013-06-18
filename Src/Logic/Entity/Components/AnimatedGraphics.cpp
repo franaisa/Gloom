e49e805a7495efb05044b8efceeefb2e6c558a76
rub0 <rub0te@gmail.com>
2013-06-18 12:42:51 +0200

    Arreglados unos cuantos materiales con errores

--- Src/Logic/Entity/Components/AnimatedGraphics.cpp
+++ Src/Logic/Entity/Components/AnimatedGraphics.cpp
@@ -187,7 +187,7 @@ namespace Logic
 			_insertAnimation = true;
 			_animatedGraphicsEntity->setAnimation( nextAnim.animation,nextAnim.loop );
 			_insertAnimation = nextAnim.exclude;
-
+			_entity->getYaw();
 			nextAnim.animation="";
 		}
 	}
