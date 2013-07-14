861e213ad3f5a50faf77853ed9b053f9d40769f7
Fran <francisco.aisa.garcia@gmail.com>
2013-07-14 19:03:23 +0200

    He cambiado los shaders de bump mapping direccional y con spot light por una version basada en el de nvidia que ese funciona sin importar la orientacion de la textura (cosa que no pasaba con el otro y ademas los especulares molan mucho mas en esta version). He comentado tambien los shaders de bump mapping que son los que mas historia tienen.

--- Exes/media/materials/programs/shadersFran/bumpMapPointLight.hlsl
+++ Exes/media/materials/programs/shadersFran/bumpMapPointLight.hlsl
@@ -1,10 +1,10 @@
 // @author Francisco Aisa García
 
 // Registros de texturas
-sampler DiffMap			: register(s0);
-sampler NormalMap	: register(s1);
+sampler DiffMap			: register(s0); // RGB = Difuso ; A = Especular
+sampler NormalMap	: register(s1); // Bump Map
 
-// Parametros de Ogre
+// Parametros proporcionados por Ogre
 float4x4 viewProjectionMatrix;
 float4 globalAmbient;
 float3 eyePosition;
@@ -12,9 +12,12 @@ float4 lightPosition;
 float4 lightColor;
 float4 lightAttenuation;
 
-// Parametros para el fragment shader
+// Parametros uniformes
+// Constante para modular el ambiente
 float Ka;
+// Constante para modular el difuso
 float Kd;
+// Potencia de brillo del especular
 float shininess;
 
 // Información de entrada del vertex shader
@@ -29,19 +32,17 @@ struct VsInput {
 struct VsOutput {
 	float4 position 			: POSITION;
 	float2 uv0 					: TEXCOORD0;
-	float4 objectPos			: TEXCOORD1;
-	float3 normal				: TEXCOORD2;
-	float3 lightDirection	: TEXCOORD3;
-	float3 halfAngle			: TEXCOORD4;
+	float3 normal				: TEXCOORD1;
+	float3 lightDirection	: TEXCOORD2;
+	float3 halfAngle			: TEXCOORD3;
 };
 
 // Información de entrada del fragment shader
 struct PsInput {
 	float2 uv0 					: TEXCOORD0;
-	float4 position			: TEXCOORD1;
-	float3 normal				: TEXCOORD2;
-	float3 lightDirection	: TEXCOORD3;
-	float3 halfAngle			: TEXCOORD4;
+	float3 normal				: TEXCOORD1;
+	float3 lightDirection	: TEXCOORD2;
+	float3 halfAngle			: TEXCOORD3;
 };
 
 //________________________________________________________________________
@@ -50,18 +51,23 @@ struct PsInput {
 VsOutput vertex_main(const VsInput IN) {
 	VsOutput OUT;
 
+	// Pasamos al fragment shader la posicion proyectada, las coordenadas de textura
+	// y las normales
 	OUT.position = mul(viewProjectionMatrix, IN.position);
 	OUT.uv0 = IN.uv0;
-	OUT.objectPos = IN.position;
 	OUT.normal = IN.normal;
 	
+	// Construimos la matriz 3x3 que nos permite pasar del espacio objeto al espacio
+	// textura (de esta forma el reflejo funciona bien sin importar la orientación
 	float3 binormal = normalize( cross(IN.normal, IN.tangent.xyz) );
 	float3x3 rotation = float3x3(IN.tangent.xyz, binormal, IN.normal);
 	
+	// Obtenemos vector director de cámara y del rayo de luz
 	float3 eyeDirection = (eyePosition - IN.position).xyz;
 	OUT.lightDirection = (lightPosition.xyz - IN.position).xyz;
 	OUT.lightDirection = normalize(OUT.lightDirection);
 	
+	// Rotamos los vectores en base a la matriz 3x3 construida anteriormente
 	eyeDirection = mul(rotation, eyeDirection);
 	OUT.lightDirection = mul(rotation, OUT.lightDirection);
 	OUT.halfAngle = normalize( normalize(OUT.lightDirection) + normalize(eyeDirection) );
@@ -71,22 +77,24 @@ VsOutput vertex_main(const VsInput IN) {
 
 //________________________________________________________________________
 
+// Las normales estan comprimidas en el espacio [0-1]. Esta función las desomprime
+// al espacio [-1, 1]
 float3 expand(float3 v) {
 	return (v - 0.5) * 2;
 }
 
-float4 compute_light(float3 N, float3 globalAmbient, float3 lightColor, float3 lightDirection, float3 halfAngle,
+float4 computeLighting(float3 N, float3 globalAmbient, float3 lightColor, float3 lightDirection, float3 halfAngle,
 							  float Ka, float Kd, float Ks, float shininess) {
 	
-	// Compute ambient term
+	// Calculamos la cantidad de luz ambiente
 	float3 ambient = Ka * globalAmbient;
 	
-	// Compute the diffuse term
+	// Calculamos la cantidad de luz ambiente basandonos en la ley del coseno de Lambert
 	float3 L = lightDirection;
 	float diffuseLight = max( dot(L, N), 0 );
 	float3 diffuse = Kd * lightColor * diffuseLight;
 	
-	// Compute the specular term
+	// Calculamos la cantidad de especular
 	float3 H = halfAngle;
 	float specularLight = pow( max(dot(H, N), 0), shininess );
 	if(diffuseLight <= 0)
@@ -102,11 +110,14 @@ float4 compute_light(float3 N, float3 globalAmbient, float3 lightColor, float3 l
 
 // FRAGMENT SHADER
 float4 fragment_main(const PsInput IN) : COLOR {
+	// Obtenemos las normales y las descomprimimos
 	float3 N = tex2D(NormalMap, IN.uv0).xyz;
 	N = expand(N);
 	N = normalize(N);
 	
-	float4 color = compute_light(N, globalAmbient, lightColor.xyz, IN.lightDirection, IN.halfAngle, Ka, Kd, tex2D(DiffMap, IN.uv0).w, shininess);
+	// Calculamos el color del pixel en base a la luz recibida
+	// Notar que la constante de especular se obtiene del canal alfa de la textura de difuso
+	float4 color = computeLighting(N, globalAmbient, lightColor.xyz, IN.lightDirection, IN.halfAngle, Ka, Kd, tex2D(DiffMap, IN.uv0).w, shininess);
 	
 	return color * float4( tex2D(DiffMap, IN.uv0).xyz, 1.0f );
 }
--- Exes/media/materials/programs/shadersFran/bumpMapSpotLight.hlsl
+++ Exes/media/materials/programs/shadersFran/bumpMapSpotLight.hlsl
@@ -1,59 +1,55 @@
 // @author Francisco Aisa García
 
 // Registros de texturas
-sampler DiffMap 		: register(s0);
-sampler NormalMap	: register(s1);
+sampler DiffMap			: register(s0); // RGB = Difuso ; A = Especular
+sampler NormalMap	: register(s1); // Bump Map
 
 // Parametros proporcionados por Ogre
 float4x4 viewProjectionMatrix;
-float4x4 worldMatrix;
-
 float4 globalAmbient;
 float3 eyePosition;
+float3 lightDir;
 float4 lightPosition;
-float3 lightDirection;
 float4 lightColor;
 float4 lightAttenuation;
 
-// Parametros para el fragment shader
+// Parametros uniformes
+// Constante para modular el ambiente
+float Ka;
+// Constante para modular el difuso
+float Kd;
+// Potencia de brillo del especular
+float shininess;
 
-// Propiedades del spotlight
+// Propiedades del spot light
 float lightCosInnerCone;
 float lightCosOuterCone;
 
-// Propiedades del material
-float Ka; // constante ambiente
-float Kd; // constante difuso
-float shininess; // brillo
-
 // Información de entrada del vertex shader
 struct VsInput {
-	float4 position 	: POSITION;
-	float3 normal 	: NORMAL;
-	float2 tex 			: TEXCOORD0;
-	float4 tangent	: TANGENT0;
-	float4 binormal	: BINORMAL0;
+	float4 position 			: POSITION;
+	float3 normal 			: NORMAL;
+	float2 uv0 					: TEXCOORD0;
+	float4 tangent			: TANGENT0;
 };
 
 // Información de salida del vertex shader
 struct VsOutput {
-	float4 position 	: POSITION;
-	float2 tex 			: TEXCOORD0;
-	float4 objectPos	: TEXCOORD1;
-	float3 normal		: TEXCOORD2;
-	float4 tangent	: TEXCOORD3;
-	float4 binormal	: TEXCOORD4;
-	float3 viewDir		: TEXCOORD5;
+	float4 position 			: POSITION;
+	float2 uv0 					: TEXCOORD0;
+	float4 objectPos			: TEXCOORD1;
+	float3 normal				: TEXCOORD2;
+	float3 lightDirection	: TEXCOORD3;
+	float3 halfAngle			: TEXCOORD4;
 };
 
 // Información de entrada del fragment shader
 struct PsInput {
-	float2 tex 			: TEXCOORD0;
-	float4 position	: TEXCOORD1;
-	float3 normal		: TEXCOORD2;
-	float4 tangent	: TEXCOORD3;
-	float4 binormal	: TEXCOORD4;
-	float3 viewDir		: TEXCOORD5;
+	float2 uv0 					: TEXCOORD0;
+	float4 position			: TEXCOORD1;
+	float3 normal				: TEXCOORD2;
+	float3 lightDirection	: TEXCOORD3;
+	float3 halfAngle			: TEXCOORD4;
 };
 
 //________________________________________________________________________
@@ -62,25 +58,27 @@ struct PsInput {
 VsOutput vertex_main(const VsInput IN) {
 	VsOutput OUT;
 
+	// Pasamos al fragment shader la posicion proyectada, las coordenadas de textura
+	// y las normales
 	OUT.position = mul(viewProjectionMatrix, IN.position);
-	OUT.tex = IN.tex;
-	
-	OUT.normal = mul(IN.normal, (float3x3)worldMatrix);
-	OUT.normal = normalize(OUT.normal);
-	
-	OUT.tangent = mul(IN.tangent, worldMatrix);
-	OUT.tangent = normalize(OUT.tangent);
-	
-	OUT.binormal = mul(IN.binormal, worldMatrix);
-	OUT.binormal = normalize(OUT.binormal);
-	
+	OUT.uv0 = IN.uv0;
 	OUT.objectPos = IN.position;
+	OUT.normal = IN.normal;
 	
-	float4 worldPosition = mul(IN.position, worldMatrix);
+	// Construimos la matriz 3x3 que nos permite pasar del espacio objeto al espacio
+	// textura (de esta forma el reflejo funciona bien sin importar la orientación
+	float3 binormal = normalize( cross(IN.normal, IN.tangent.xyz) );
+	float3x3 rotation = float3x3(IN.tangent.xyz, binormal, IN.normal);
 	
-	OUT.viewDir = eyePosition.xyz - worldPosition.xyz;
+	// Obtenemos vector director de cámara y del rayo de luz
+	float3 eyeDirection = (eyePosition - IN.position).xyz;
+	OUT.lightDirection = (lightPosition.xyz - IN.position).xyz;
+	OUT.lightDirection = normalize(OUT.lightDirection);
 	
-	OUT.viewDir = normalize(OUT.viewDir);
+	// Rotamos los vectores en base a la matriz 3x3 construida anteriormente
+	eyeDirection = mul(rotation, eyeDirection);
+	OUT.lightDirection = mul(rotation, OUT.lightDirection);
+	OUT.halfAngle = normalize( normalize(OUT.lightDirection) + normalize(eyeDirection) );
 
 	return OUT;
 }
@@ -92,42 +90,53 @@ float dualConeSpotlight(float3 P) {
 	float cosOuterCone = lightCosOuterCone;
 	float cosInnerCone = lightCosInnerCone;
 
-	float cosDirection = dot(V, normalize(lightDirection)); // In case the direction is not normalized
+	float cosDirection = dot(V, normalize(lightDir));
 	return smoothstep(cosOuterCone, cosInnerCone, cosDirection);
+}
+
+// Las normales estan comprimidas en el espacio [0-1]. Esta función las desomprime
+// al espacio [-1, 1]
+float3 expand(float3 v) {
+	return (v - 0.5) * 2;
+}
+
+float4 computeLighting(float3 P, float3 N, float3 globalAmbient, float3 lightColor, float3 lightDirection, float3 halfAngle, float Ka, float Kd, float Ks, float shininess) {
+	
+	// Calculamos el efecto del sopt light
+	float spotEffect = dualConeSpotlight(P);
+	
+	// Calculamos la cantidad de luz ambiente
+	float3 ambient = Ka * globalAmbient;
+	
+	// Calculamos la cantidad de luz ambiente basandonos en la ley del coseno de Lambert
+	float3 L = lightDirection;
+	float diffuseLight = max( dot(L, N), 0 );
+	float3 diffuse = Kd * spotEffect * lightColor * diffuseLight;
+	
+	// Calculamos la cantidad de especular
+	float3 H = halfAngle;
+	float specularLight = pow( max(dot(H, N), 0), shininess );
+	if(diffuseLight <= 0)
+		specularLight = 0;
+		
+	float3 specular = Ks * spotEffect * lightColor * specularLight;
+	
+	float3 color = ambient + diffuse + specular;
+	return float4(color, 1.0f);
 }
 
 //________________________________________________________________________
 
 // FRAGMENT SHADER
 float4 fragment_main(const PsInput IN) : COLOR {
-	float4 textureColor = float4( tex2D(DiffMap, IN.tex).xyz, 1.0f );
-	float4 bumpMap = tex2D(NormalMap, IN.tex);
-
-	bumpMap = (bumpMap * 2.0f) - 1.0f;
+	// Obtenemos las normales y las descomprimimos
+	float3 N = tex2D(NormalMap, IN.uv0).xyz;
+	N = expand(N);
+	N = normalize(N);
 	
-	float3 bumpNormal = IN.normal + bumpMap.x * IN.tangent.xyz + bumpMap.y * IN.binormal.xyz;
-	bumpNormal = normalize(bumpNormal);
+	// Calculamos el color del pixel en base a la luz recibida
+	// Notar que la constante de especular se obtiene del canal alfa de la textura de difuso
+	float4 color = computeLighting(IN.position.xyz, N, globalAmbient, lightColor.xyz, IN.lightDirection, IN.halfAngle, Ka, Kd, tex2D(DiffMap, IN.uv0).w, shininess);
 	
-	float3 lightDir = -lightDirection;
-	
-	float spotEffect = dualConeSpotlight( IN.position.xyz );
-	
-	// CALCULAMOS EL DIFUSO
-	float diffuseLight = saturate( dot(bumpNormal, lightDir) );
-	float3 diffuseTerm = Kd * spotEffect * lightColor.xyz * diffuseLight;
-	
-	// CALCULAMOS EL AMBIENTE
-	float3 ambientTerm = Ka * globalAmbient.xyz;
-		
-	// CALCULAMOS EL ESPECULAR
-	float specularIntensity = tex2D(DiffMap, IN.tex).w;
-	
-	float3 reflection = normalize( 2 * diffuseLight * bumpNormal - lightDir );
-	
-	float specularLight = pow( saturate( dot(reflection, IN.viewDir) ), shininess );
-	float3 specularTerm =  specularIntensity * spotEffect * lightColor.xyz * specularLight;
-	
-	float3 color = saturate( ambientTerm + diffuseTerm + specularTerm );
-
-	return float4(color, 1.0f) * textureColor;
+	return color * float4( tex2D(DiffMap, IN.uv0).xyz, 1.0f );
 }
